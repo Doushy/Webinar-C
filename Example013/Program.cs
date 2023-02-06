03cb63e4a34cc785b4a664e0e89dde45c592c05e
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-06 15:19:22 +0300

    add 12,13,14

---
+++ Example013/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int num = new Random().Next(10,100);
+int a1 = num/10;
+int a2 = num%10;
+int max = a1;
+if(max < a2)
+{
+    max = a2;
+}
+Console.WriteLine(num);
+Console.WriteLine(a1);
+Console.WriteLine(a2);
+Console.WriteLine(max);
